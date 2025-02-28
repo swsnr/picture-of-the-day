0ff64f80d68de306227a3a96318e2099d28570ba
Sebastian Wiesner <sebastian@swsnr.de>
2025-02-28 23:28:00 +0100

    Drop EOPOD source for now
    It's harder to port correctly since it doesn't provide HQ images in a
    machine-readable form, and I'm not using this source myself.

--- src/source.rs
+++ src/source.rs
@@ -10,7 +10,6 @@
 #![allow(clippy::as_conversions)]
 
 use glib::{GString, dpgettext2};
-use gtk::gio::IOErrorEnum;
 
 use crate::config::G_LOG_DOMAIN;
 use crate::image::DownloadableImage;
@@ -40,7 +39,6 @@ pub use error::SourceError;
 #[strum(serialize_all = "kebab-case")]
 pub enum Source {
     Apod,
-    Eopod,
     Bing,
     Wikimedia,
     Stalenhag,
@@ -58,11 +56,6 @@ impl Source {
     pub fn i18n_name(self) -> GString {
         match self {
             Source::Apod => dpgettext2(None, "source name", "NASA Astronomy Picture Of The Day"),
-            Source::Eopod => dpgettext2(
-                None,
-                "source name",
-                "NASA Earth Observatory Image Of The Day",
-            ),
             Source::Bing => dpgettext2(None, "source name", "Bing"),
             Source::Wikimedia => dpgettext2(None, "source name", "Wikimedia Picture Of The Day"),
             Source::Stalenhag => dpgettext2(None, "source name", "Simon Stålenhag"),
@@ -76,7 +69,6 @@ impl Source {
     pub fn url(self) -> &'static str {
         match self {
             Source::Apod => "https://apod.nasa.gov/",
-            Source::Eopod => "https://earthobservatory.nasa.gov/topic/image-of-the-day",
             Source::Bing => "https://bing.com",
             Source::Wikimedia => "https://commons.wikimedia.org/wiki/Main_Page",
             Source::Stalenhag => "https://simonstalenhag.se/",
@@ -87,19 +79,12 @@ impl Source {
         self,
         session: &soup::Session,
     ) -> Result<Vec<DownloadableImage>, SourceError> {
-        let not_supported: SourceError = glib::Error::new(
-            IOErrorEnum::NotSupported,
-            &format!("Source {self:?} not supported yet!"),
-        )
-        .into();
-        #[allow(clippy::match_same_arms)]
         let images = match self {
-            Source::Apod => Ok(vec![apod::fetch_picture_of_the_day(session).await?]),
-            Source::Eopod => Err(not_supported),
-            Source::Bing => Ok(bing::fetch_daily_images(session).await?),
-            Source::Wikimedia => Ok(vec![wikimedia::fetch_featured_image(session).await?]),
-            Source::Stalenhag => Ok(vec![stalenhag::pick_todays_image()]),
-        }?;
+            Source::Apod => vec![apod::fetch_picture_of_the_day(session).await?],
+            Source::Bing => bing::fetch_daily_images(session).await?,
+            Source::Wikimedia => vec![wikimedia::fetch_featured_image(session).await?],
+            Source::Stalenhag => vec![stalenhag::pick_todays_image()],
+        };
 
         if images.is_empty() {
             glib::warn!("Source {self:?} returned an empty list of images!");
