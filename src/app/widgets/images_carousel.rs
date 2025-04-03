31c9c97bec88094fe2e2c4b404b2678c6138f404
Sebastian Wiesner <sebastian@swsnr.de>
2025-04-03 09:08:07 +0200

    Add proper navigation bar for image carousel
    Move the carousel indicator into an overlay OSD toolbar, and add
    navigation buttons.
    
    Add arrow key and page key shortcuts to go to the previous/next image.
    
    Make the navbar appear automatically when these shortcuts are used, or
    when the pointer moves in the window. Hide the navbar automatically
    after a three seconds, or when the pointer leaves the window.
    
    While adding the navbar, also move outer margins to the main window, for
    consistency.
    
    Closes GH-18

--- src/app/widgets/images_carousel.rs
+++ src/app/widgets/images_carousel.rs
@@ -22,6 +22,14 @@ impl ImagesCarousel {
     pub fn set_images(&self, images: &[Image]) {
         self.imp().set_images(images);
     }
+
+    pub fn scroll_next_image(&self, animate: bool) {
+        self.imp().scroll_image_offset(1, animate);
+    }
+
+    pub fn scroll_previous_image(&self, animate: bool) {
+        self.imp().scroll_image_offset(-1, animate);
+    }
 }
 
 impl Default for ImagesCarousel {
@@ -31,12 +39,16 @@ impl Default for ImagesCarousel {
 }
 
 mod imp {
-    use std::{cell::RefCell, cmp::Ordering};
+    use std::cell::{Cell, RefCell};
+    use std::cmp::Ordering;
 
     use adw::prelude::*;
     use adw::subclass::prelude::*;
-    use glib::{Properties, subclass::InitializingObject};
-    use gtk::CompositeTemplate;
+    use glib::{Properties, SourceId, subclass::InitializingObject};
+    use gtk::{
+        CompositeTemplate,
+        gdk::{Key, ModifierType},
+    };
 
     use crate::app::{model::Image, widgets::ImagePage};
 
@@ -46,13 +58,37 @@ mod imp {
     pub struct ImagesCarousel {
         #[property(get)]
         current_image: RefCell<Option<Image>>,
+        /// Whether to show navigation.
+        ///
+        /// When `true` show navigation, but only if the carousel has more than one page.
+        #[property(get, set = Self::set_show_nav)]
+        show_nav: Cell<bool>,
         #[template_child]
-        images_carousel: TemplateChild<adw::Carousel>,
+        carousel: TemplateChild<adw::Carousel>,
+        nav_autohide_timeout: RefCell<Option<SourceId>>,
+    }
+
+    #[gtk::template_callbacks]
+    impl ImagesCarousel {
+        #[template_callback(function)]
+        fn show_nav(number_of_pages: u32, show_nav: bool) -> bool {
+            show_nav && 1 < number_of_pages
+        }
+
+        #[template_callback]
+        fn pointer_enter_or_move(&self) {
+            self.flash_nav_bar();
+        }
+
+        #[template_callback]
+        fn pointer_leave(&self) {
+            self.obj().set_show_nav(false);
+        }
     }
 
     impl ImagesCarousel {
         pub fn nth_image(&self, n: u32) -> Image {
-            self.images_carousel
+            self.carousel
                 .nth_page(n)
                 .downcast::<ImagePage>()
                 .unwrap()
@@ -61,11 +97,63 @@ mod imp {
                 .unwrap()
         }
 
+        fn set_show_nav(&self, show_nav: bool) {
+            self.show_nav.set(show_nav);
+            if show_nav {
+                // Cancel any auto-hide timer
+                if let Some(source_id) = self.nav_autohide_timeout.take() {
+                    source_id.remove();
+                }
+            }
+        }
+
+        /// Show navigation bar for a few seconds.
+        fn flash_nav_bar(&self) {
+            self.obj().set_show_nav(true);
+            self.nav_autohide_timeout
+                .replace(Some(glib::timeout_add_seconds_local_once(
+                    3,
+                    glib::clone!(
+                        #[weak(rename_to = carousel)]
+                        self.obj(),
+                        move || {
+                            carousel.set_show_nav(false);
+                            carousel.imp().nav_autohide_timeout.replace(None);
+                        }
+                    ),
+                )));
+        }
+
+        /// Scroll to the n'th page.
+        fn scroll_to_nth_page(&self, n: u32, animate: bool) {
+            let n_pages = self.carousel.n_pages();
+            if n_pages == 0 {
+                return;
+            }
+            let n = n.clamp(0, n_pages - 1);
+            let page = self.carousel.nth_page(n);
+            self.carousel.scroll_to(&page, animate);
+        }
+
+        pub fn scroll_image_offset(&self, offset: i32, animate: bool) {
+            let position = if 0 < offset {
+                self.carousel.position().floor()
+            } else {
+                self.carousel.position().ceil()
+            };
+            // A carousel only has u32::MAX pages at most, so this won't truncate.
+            #[allow(clippy::cast_possible_truncation, clippy::as_conversions)]
+            let position = position as i64;
+            let new_position = (position + i64::from(offset)).max(0).try_into().unwrap();
+            self.scroll_to_nth_page(new_position, animate);
+            self.flash_nav_bar();
+        }
+
         /// Set images to show.
         ///
         /// Create as many pages as there are `images`, all in loading state.
         pub fn set_images(&self, images: &[Image]) {
-            let carousel = self.images_carousel.get();
+            let carousel = self.carousel.get();
 
             let n_pages = usize::try_from(carousel.n_pages()).unwrap();
             match n_pages.cmp(&images.len()) {
@@ -95,7 +183,7 @@ mod imp {
             }
 
             // Then navigate to first page
-            self.images_carousel
+            self.carousel
                 .scroll_to(&carousel.nth_page(0), adw::is_animations_enabled(&carousel));
             self.update_current_image(0);
         }
@@ -116,6 +204,23 @@ mod imp {
 
         fn class_init(klass: &mut Self::Class) {
             klass.bind_template();
+            klass.bind_template_callbacks();
+
+            klass.install_action("image.next", None, |carousel, _, _| {
+                carousel.scroll_next_image(false);
+            });
+            klass.install_action("image.previous", None, |carousel, _, _| {
+                carousel.scroll_previous_image(false);
+            });
+
+            klass.add_binding_action(Key::Left, ModifierType::NO_MODIFIER_MASK, "image.previous");
+            klass.add_binding_action(
+                Key::Page_Up,
+                ModifierType::NO_MODIFIER_MASK,
+                "image.previous",
+            );
+            klass.add_binding_action(Key::Right, ModifierType::NO_MODIFIER_MASK, "image.next");
+            klass.add_binding_action(Key::Page_Down, ModifierType::NO_MODIFIER_MASK, "image.next");
         }
 
         fn instance_init(obj: &InitializingObject<Self>) {
@@ -126,7 +231,7 @@ mod imp {
     #[glib::derived_properties]
     impl ObjectImpl for ImagesCarousel {
         fn constructed(&self) {
-            self.images_carousel.connect_page_changed(glib::clone!(
+            self.carousel.connect_page_changed(glib::clone!(
                 #[weak(rename_to = carousel)]
                 self.obj(),
                 move |_, n| {
